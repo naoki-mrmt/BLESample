32fb4a8743f77a38188e0577ab360dcc824f38a4
Naoki <20cm0135@jec.ac.jp>
2022-03-02 09:39:49 +0900

    Initial Commit

---
+++ BLESample/BLESampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESampleApp.swift
+//  BLESample
+//
+//  Created by cmStudent on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct BLESampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
